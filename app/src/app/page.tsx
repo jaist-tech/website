bcf3fb7daf143881fe1acb7e4c9540f8eba9d4b2
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2025-12-09 12:22:10 +0900

    fix: rename Home component to Top

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,9 @@
 import { HeroSection } from "@/components/top/HeroSection";
-
 import { ja } from "@/locales/ja";
 
 const { top } = ja;
 
-export default function Home() {
+export default function Top() {
   return (
       <main>
         <HeroSection
